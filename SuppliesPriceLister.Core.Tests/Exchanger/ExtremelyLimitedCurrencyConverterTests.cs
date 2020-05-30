f5d7335dbab5e58ce304729a116c5664adbd4ad6
Robbie Hameeteman <hameeteman.robert@gmail.com>
2020-05-30 13:07:39 +1000

    Test Project & exhange converter tests

---
+++ SuppliesPriceLister.Core.Tests/Exchanger/ExtremelyLimitedCurrencyConverterTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using SuppliesPriceLister.Core.Exchanger;
+using SuppliesPriceLister.Core.Ioc;
+using Xunit;
+
+namespace SuppliesPriceLister.Core.Tests.Exchanger
+{
+    public class ExtremelyLimitedCurrencyConverterTests
+    {
+        [Fact]
+        public async Task GivenConvertCurrency_WhenCurrenciesAreSame_ThenNoConversionHappens()
+        {
+            var options = new OptionsWrapper<CurrencyConfig>(new CurrencyConfig { AudUsdExchangeRate = 10 });
+            var convert = new ExtremelyLimitedCurrencyConverter(options);
+
+            var currency = await convert.ConvertCurrency(10.3m, "AUD", "AUD");
+
+            currency.Should().Be(10.3m);
+
+            currency = await convert.ConvertCurrency(323.53m, "CAD", "CAD");
+
+            currency.Should().Be(323.53m);
+
+            currency = await convert.ConvertCurrency(210.3m, "USD", "USD");
+
+            currency.Should().Be(210.3m);
+        }
+
+        [Fact]
+        public async Task GivenConvertCurrency_WhenCurrencyFromIsAUDAndToIsUSD_ThenConversionHappens()
+        {
+            var options = new OptionsWrapper<CurrencyConfig>(new CurrencyConfig { AudUsdExchangeRate = 10 });
+            var convert = new ExtremelyLimitedCurrencyConverter(options);
+            
+            var currency = await convert.ConvertCurrency(10.3m, "USD", "AUD");
+
+            currency.Should().Be(103.0m);
+
+            currency = await convert.ConvertCurrency(33.12m, "USD", "AUD");
+
+            currency.Should().Be(331.2m);
+
+            options = new OptionsWrapper<CurrencyConfig>(new CurrencyConfig { AudUsdExchangeRate = 5 });
+            convert = new ExtremelyLimitedCurrencyConverter(options);
+
+            currency = await convert.ConvertCurrency(5m, "USD", "AUD");
+
+            currency.Should().Be(25m);
+        }
+
+        [Fact]
+        public async Task GivenConvertCurrency_WhenCurrencyIsNotAudToUSD_ThenArgumentExceptionExplosion()
+        {
+            var options = new OptionsWrapper<CurrencyConfig>(new CurrencyConfig { AudUsdExchangeRate = 10 });
+            var convert = new ExtremelyLimitedCurrencyConverter(options);
+
+            await convert.Invoking(async y => await y.ConvertCurrency(10.3m, "CAD", "AUD"))
+                .Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Can only perform USD to AUD conversions 😥");
+
+            await convert.Invoking(async y => await y.ConvertCurrency(10.3m, "AUD", "USD"))
+                .Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Can only perform USD to AUD conversions 😥");
+
+            await convert.Invoking(async y => await y.ConvertCurrency(10.3m, "CAD", "USD"))
+                .Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Can only perform USD to AUD conversions 😥");
+        }
+    }
+}
--- SuppliesPriceLister.Core/Ioc/SuppliesPriceListerCoreBindings.cs
+++ SuppliesPriceLister.Core/Ioc/SuppliesPriceListerCoreBindings.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SuppliesPriceLister.Core.Exchanger;
@@ -7,6 +8,9 @@ using SuppliesPriceLister.Core.Loaders;
 using SuppliesPriceLister.Core.Loaders.Csv;
 using SuppliesPriceLister.Core.Loaders.Json;
 using SuppliesPriceLister.Core.Processors;
+
+[assembly: InternalsVisibleTo("SuppliesPriceLister.Core.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
 namespace SuppliesPriceLister.Core.Ioc
 {
